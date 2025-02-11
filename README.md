e9e82666ecd165320f00628dc9c72382100ff75b
C03Player <C03PacketPlayer@proton.me>
2025-02-11 16:12:48 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# MinibloxTranslationLayer
+A middle man to translate Miniblox packets into Minecraft 1.8.9 packets.
+
+## Modifications
+I changed the version number that gets sent to the server to bypass their checks. I will also continue to bypass their checks if/when they create new ones
+
+## Use Steps
+1. Install the latest NodeJS at (https://nodejs.org/)
+2. Download & extract the repository to a random folder
+3. Open a terminal inside said folder
+4. Run npm install & node index.js
+5. Connect to localhost on a supported Minecraft 1.8.9 client.
+
+## Commands
+/play (gamemode type / eggwars / skywars / kitpvp) (aliases include /queue)
---
+++ miniblox/utils.js
@@ -0,0 +1,104 @@
+const { PBItemStack } = require('./main.js');
+const ITEMS = require('./types/items.js');
+const { COLOR_PALETTE, COLOR_REGEX, COLOR_CODES, LEVEL_TO_COLOUR } = require('./types/colors.js');
+
+function colorDistance(color1, color2) {
+	const rgb1 = hexToRgb(color1);
+	const rgb2 = hexToRgb(color2);
+	return Math.sqrt(
+		Math.pow(rgb1.r - rgb2.r, 2) +
+		Math.pow(rgb1.g - rgb2.g, 2) +
+		Math.pow(rgb1.b - rgb2.b, 2)
+	);
+}
+
+function findClosestColor(hex) {
+	let closestColor = null;
+	let closestDistance = Infinity;
+	for (const color in COLOR_PALETTE) {
+		const distance = colorDistance(hex, color);
+		if (distance < closestDistance) {
+			closestDistance = distance;
+			closestColor = color;
+		}
+	}
+	return COLOR_PALETTE[closestColor];
+}
+
+function hexToRgb(hex) {
+	const bigint = parseInt(hex.slice(1), 16);
+	return {
+		r: (bigint >> 16) & 255,
+		g: (bigint >> 8) & 255,
+		b: bigint & 255
+	};
+}
+
+function translateItem(item) {
+	let data;
+	if (item.data) {
+		let parsed = JSON.parse(item.data);
+		if (parsed.ench) {
+			let enchants = [];
+			for (const ench of parsed.ench) {
+				enchants.push({lvl: {type: "short", value: ench.lvl}, id: {type: "short", value: ench.id}});
+			}
+			data = {
+				name: "",
+				type: "compound",
+				value: {
+					ench: {
+						type: "list",
+						value: {
+							type: "compound",
+							value: enchants
+						}
+					}
+				}
+			};
+		}
+	}
+
+	const itemData = item.present && (ITEMS[item.id] ?? 166);
+	return item.present ? {
+		blockId: typeof itemData == 'number' ? itemData : itemData[0],
+		itemCount: item.stackSize,
+		itemDamage: (typeof itemData == 'number' ? item.durability : itemData[1]),
+		nbtData: data
+	} : {blockId: -1}
+}
+
+function translateItemBack(item) {
+	let itemId;
+	let data = void 0;
+	for (const [mini, mc] of Object.entries(ITEMS)) {
+		const compared = typeof mc == 'number' ? mc : mc[0];
+		if (item.blockId === compared && item != 166) {
+			itemId = Number.parseInt(mini);
+			break;
+		}
+	}
+
+	if (item.nbtData && item.nbtData.value.ench) {
+		data = {ench: []};
+		for (const ench of item.nbtData.value.ench.value.value) {
+			data.ench.push({id: ench.id.value, lvl: ench.lvl.value});
+		}
+		data = JSON.stringify(data);
+	}
+
+	return itemId != undefined ? new PBItemStack({
+		present: true,
+		id: itemId,
+		stackSize: item.itemCount,
+		durability: Math.floor(item.itemDamage),
+		data: data
+	}) : new PBItemStack({present: false});
+}
+
+function translateText(text) {
+	for (const [code, color] of Object.entries(COLOR_CODES)) text = text.replaceAll(code, color);
+	return text.replaceAll(COLOR_REGEX, (match) => {return findClosestColor(match.replaceAll("\\",''))});
+}
+
+module.exports = { translateItem, translateItemBack, translateText, LEVEL_TO_COLOUR };
